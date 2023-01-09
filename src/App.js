cccc51673b463e791c82e637d6ec0958e94be8f8
Vladimir RM <shvdrummer@gmail.com>
2023-01-09 10:37:42 +0200

    add main state tank,mpg,distance

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+const [tank,setTank]= useState(0)
+const [mpg,setMpg]= useState(0)
+const [distance,setDistance]= useState(0)
   return (
     <div className="App">
    
