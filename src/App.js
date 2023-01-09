8ea6d482aa94e0bb48b3ec67a854136740f707e1
Vladimir RM <shvdrummer@gmail.com>
2023-01-09 10:40:12 +0200

    adding const onTankChange

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ function App() {
 const [tank,setTank]= useState(0)
 const [mpg,setMpg]= useState(0)
 const [distance,setDistance]= useState(0)
+
+
+
+const onTankChange = ()=>{
+  
+}
   return (
     <div className="App">
+      <input value={tank} onChange={onTankChange} />
    
     </div>
   );
